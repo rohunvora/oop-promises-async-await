f25503e0d901ada3f4fea42c1e29acf3c26a858e
Rohun Vora <emailrohun@gmail.com>
2020-07-23 14:45:01 -0700

    [Constructor Function]Add Artists

--- app.js
+++ app.js
@@ -31,3 +31,39 @@ const dogFour = {
 }
 
 dogFour.__proto__ = dogThree;
+
+// Constructor Functions
+function User(name) {
+  this.name = name;
+}
+
+let adam = User('Adam');
+let pete = new User('Pete');
+console.log(adam);
+
+function NBAPlayer(name, team, threePointShooter) {
+  this.name = name;
+  this.team = team;
+  this.threePointShooter = threePointShooter;
+}
+
+let steph = new NBAPlayer('Steph Curry', 'Warriors', true);
+console.log(steph);
+console.log(steph.name);
+
+
+function Artist(name) {
+  this.name = name;
+}
+
+function bestArtist(name, album, ranking) {
+  this.name = name;
+  this.album = album;
+  this.ranking = ranking;
+}
+
+let playboi = new bestArtist('Playboi Carti', 'Die Lit', 1);
+let frank = Artist('Frank Ocean', 'Blonde', 2);
+
+console.log(playboi);
+console.log(playboi.name);
